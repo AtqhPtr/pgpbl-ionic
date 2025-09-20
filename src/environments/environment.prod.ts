ebae7391d98dbc81da4989d2597415d1bca3f8fd
AtqhPtr <putriistiatika13@gmail.com>
2025-09-20 09:57:02 +0700

    Acara 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Your web app's Firebase configuration
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHL6f2cwJ7SYnuXIC87hP_Ovz1lugZ8wc',
+    authDomain: 'pgpbl-ionic-atika.firebaseapp.com',
+    projectId: 'pgpbl-ionic-atika',
+    storageBucket: 'pgpbl-ionic-atika.firebasestorage.app',
+    messagingSenderId: '489545021509',
+    appId: '1:489545021509:web:d6de6180d3fc9bb2624b2c',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Your web app's Firebase configuration
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHL6f2cwJ7SYnuXIC87hP_Ovz1lugZ8wc',
+    authDomain: 'pgpbl-ionic-atika.firebaseapp.com',
+    projectId: 'pgpbl-ionic-atika',
+    storageBucket: 'pgpbl-ionic-atika.firebasestorage.app',
+    messagingSenderId: '489545021509',
+    appId: '1:489545021509:web:d6de6180d3fc9bb2624b2c',
+  },
 };
 
 /*
